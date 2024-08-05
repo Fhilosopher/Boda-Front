4da017c612dba48d18e44ddb5a4d467412534724
SEOA <tjdk2002@gmail.com>
2024-08-06 01:26:58 +0900

    0805 되돌리기

--- public/serviceWorker.js
+++ public/serviceWorker.js
@@ -1,31 +1,15 @@
 self.addEventListener("push", function (event) {
-  let data = event.data.json();
+  const data = event.data.json();
   const title = data.title || "알림";
-
-  try {
-    data = event.data.json();
-  } catch (e) {
-    data = { title: "Notification", body: event.data.text() };
-  }
-
   const options = {
     body: data.body,
-    icon: data.icon || "https://i.imgur.com/J490BGB.png",
-    badge: data.badge || "https://i.imgur.com/NCVXfsQ.png",
+    icon: data.icon,
+    badge: data.badge,
     image: data.image,
     data: {
       url: data.url,
     },
   };
 
-  event.waitUntil(self.registration.showNotification(data.title, options));
-});
-
-self.addEventListener("notificationclick", function (event) {
-  event.notification.close();
-  event.waitUntil(
-    clients.openWindow(
-      event.notification.data.url || "https://bodaessay.vercel.app/"
-    )
-  );
+  event.waitUntil(self.registration.showNotification(title, options));
 });
