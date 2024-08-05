a549ab80bd6292adcf0294c8d733028f5832d840
SEOA <tjdk2002@gmail.com>
2024-08-06 01:05:56 +0900

    0805 웹푸시 on할 때 요청, 서비스워커 로고 추가

--- public/serviceWorker.js
+++ public/serviceWorker.js
@@ -1,15 +1,31 @@
 self.addEventListener("push", function (event) {
-  const data = event.data.json();
+  let data = event.data.json();
   const title = data.title || "알림";
+
+  try {
+    data = event.data.json();
+  } catch (e) {
+    data = { title: "Notification", body: event.data.text() };
+  }
+
   const options = {
     body: data.body,
-    icon: data.icon,
-    badge: data.badge,
+    icon: data.icon || "https://i.imgur.com/J490BGB.png",
+    badge: data.badge || "https://i.imgur.com/NCVXfsQ.png",
     image: data.image,
     data: {
       url: data.url,
     },
   };
 
-  event.waitUntil(self.registration.showNotification(title, options));
+  event.waitUntil(self.registration.showNotification(data.title, options));
+});
+
+self.addEventListener("notificationclick", function (event) {
+  event.notification.close();
+  event.waitUntil(
+    clients.openWindow(
+      event.notification.data.url || "https://bodaessay.vercel.app/"
+    )
+  );
 });
--- src/component/myInfo/myInfoDetail.js
+++ src/component/myInfo/myInfoDetail.js
@@ -53,136 +53,131 @@ function MyInfoDetail() {
       }
     };
 
-    const handlePushNotification = async () => {
-      if ("Notification" in window && "serviceWorker" in navigator) {
-        try {
-          // 알림 권한 요청
-          const permission = await Notification.requestPermission();
-          if (permission === "granted") {
-            // Service Worker 등록 확인
-            const registration =
-              await navigator.serviceWorker.getRegistration();
-            if (registration) {
-              // 푸시 구독 확인
-              const subscription =
-                await registration.pushManager.getSubscription();
-              if (subscription) {
-                // 이미 구독된 경우, endpoint와 key를 추출
-                const endpoint = subscription.endpoint;
-                const key = subscription.getKey("p256dh");
-                const auth = subscription.getKey("auth");
+    fetchMyInfo();
+  }, []);
 
-                const updatedSubscribe = {
-                  endpoint: endpoint,
-                  p256dh: arrayBufferToBase64(key),
-                  auth: arrayBufferToBase64(auth),
-                };
-                setSubscribe(updatedSubscribe);
-                sendPushNotification(updatedSubscribe);
+  const handlePushNotification = async () => {
+    if ("Notification" in window && "serviceWorker" in navigator) {
+      try {
+        // 알림 권한 요청
+        const permission = await Notification.requestPermission();
+        if (permission === "granted") {
+          // Service Worker 등록 확인
+          const registration = await navigator.serviceWorker.getRegistration();
+          if (registration) {
+            // 푸시 구독 확인
+            const subscription =
+              await registration.pushManager.getSubscription();
+            if (subscription) {
+              // 이미 구독된 경우, endpoint와 key를 추출
+              const endpoint = subscription.endpoint;
+              const key = subscription.getKey("p256dh");
+              const auth = subscription.getKey("auth");
 
-                console.log("기존 푸시 구독 정보가 존재합니다");
-                console.log("Endpoint:", endpoint);
-                console.log("p256dh:", key ? arrayBufferToBase64(key) : "없음");
-                console.log("Auth:", auth ? arrayBufferToBase64(auth) : "없음");
-              } else {
-                // 구독이 없는 경우 새로 구독 생성
-                const newSubscription =
-                  await registration.pushManager.subscribe({
-                    userVisibleOnly: true,
-                    applicationServerKey: urlBase64ToUint8Array(
-                      "BKd1I8PDrg7zidthfuKgR-0k_-CwlXZk3p1_F1tLWAur3w3lDdSfiAQHFl_cXRi6pguzNe9Akfxrqo25XXXxGZ4"
-                    ), // VAPID 공개 키
-                  });
+              const updatedSubscribe = {
+                endpoint: endpoint,
+                p256dh: arrayBufferToBase64(key),
+                auth: arrayBufferToBase64(auth),
+              };
+              setSubscribe(updatedSubscribe);
+              sendPushNotification(updatedSubscribe);
 
-                const endpoint = newSubscription.endpoint;
-                const key = newSubscription.getKey("p256dh");
-                const auth = newSubscription.getKey("auth");
-
-                const updatedSubscribe = {
-                  endpoint: endpoint,
-                  p256dh: arrayBufferToBase64(key),
-                  auth: arrayBufferToBase64(auth),
-                };
-                setSubscribe(updatedSubscribe);
-                sendPushNotification(updatedSubscribe);
-
-                console.log("새로운 푸시 구독 정보를 등록하였습니다.");
-                console.log("Endpoint:", endpoint);
-                console.log("p256dh:", key ? arrayBufferToBase64(key) : "없음");
-                console.log("Auth:", auth ? arrayBufferToBase64(auth) : "없음");
-              }
+              console.log("기존 푸시 구독 정보가 존재합니다");
+              console.log("Endpoint:", endpoint);
+              console.log("p256dh:", key ? arrayBufferToBase64(key) : "없음");
+              console.log("Auth:", auth ? arrayBufferToBase64(auth) : "없음");
             } else {
-              alert("서비스 워커가 등록되지 않았습니다.");
+              // 구독이 없는 경우 새로 구독 생성
+              const newSubscription = await registration.pushManager.subscribe({
+                userVisibleOnly: true,
+                applicationServerKey: urlBase64ToUint8Array(
+                  "BKd1I8PDrg7zidthfuKgR-0k_-CwlXZk3p1_F1tLWAur3w3lDdSfiAQHFl_cXRi6pguzNe9Akfxrqo25XXXxGZ4"
+                ), // VAPID 공개 키
+              });
+
+              const endpoint = newSubscription.endpoint;
+              const key = newSubscription.getKey("p256dh");
+              const auth = newSubscription.getKey("auth");
+
+              const updatedSubscribe = {
+                endpoint: endpoint,
+                p256dh: arrayBufferToBase64(key),
+                auth: arrayBufferToBase64(auth),
+              };
+              setSubscribe(updatedSubscribe);
+              sendPushNotification(updatedSubscribe);
+
+              console.log("새로운 푸시 구독 정보를 등록하였습니다.");
+              console.log("Endpoint:", endpoint);
+              console.log("p256dh:", key ? arrayBufferToBase64(key) : "없음");
+              console.log("Auth:", auth ? arrayBufferToBase64(auth) : "없음");
             }
           } else {
-            alert("알림 권한이 거부되었습니다.");
+            alert("서비스 워커가 등록되지 않았습니다.");
           }
-        } catch (error) {
-          console.error("푸시 알림 처리 중 오류 발생:", error);
-        }
-      } else {
-        alert(
-          "이 브라우저는 알림을 지원하지 않거나, 서비스 워커를 지원하지 않습니다."
-        );
-      }
-    };
-
-    const sendPushNotification = async (updatedSubscribe) => {
-      const headers = {
-        Authorization: `Bearer ${localStorage.getItem("accessToken")}`,
-      };
-      const body = {
-        endpoint: updatedSubscribe.endpoint,
-        keys: {
-          p256dh: updatedSubscribe.p256dh,
-          auth: updatedSubscribe.auth,
-        },
-      };
-      try {
-        const res = await instance.post("webmessage/subscribe/", body, {
-          headers,
-        });
-        // const res = {
-        //   message: "Subscription saved.",
-        // };
-        if (res.status === 200) {
-          console.log("구독정보 전송 완료");
         } else {
-          console.log("구독정보 전송 실패");
+          alert("브라우저 알림 권한이 거부되어 있습니다.");
         }
-      } catch (err) {
-        alert(err);
+      } catch (error) {
+        console.error("푸시 알림 처리 중 오류 발생:", error);
       }
-    };
+    } else {
+      alert(
+        "이 브라우저는 알림을 지원하지 않거나, 서비스 워커를 지원하지 않습니다."
+      );
+    }
+  };
 
-    // ArrayBuffer를 Base64로 변환하는 함수
-    const arrayBufferToBase64 = (buffer) => {
-      let binary = "";
-      const bytes = new Uint8Array(buffer);
-      const len = bytes.byteLength;
-      for (let i = 0; i < len; i++) {
-        binary += String.fromCharCode(bytes[i]);
+  const sendPushNotification = async (updatedSubscribe) => {
+    const headers = {
+      Authorization: `Bearer ${localStorage.getItem("accessToken")}`,
+    };
+    const body = {
+      endpoint: updatedSubscribe.endpoint,
+      keys: {
+        p256dh: updatedSubscribe.p256dh,
+        auth: updatedSubscribe.auth,
+      },
+    };
+    try {
+      const res = await instance.post("webmessage/subscribe/", body, {
+        headers,
+      });
+      if (res.status === 200) {
+        console.log("구독정보 전송 완료");
+        handlePushOnOff();
+      } else {
+        console.log("구독정보 전송 실패");
       }
-      return window.btoa(binary);
-    };
+    } catch (err) {
+      alert(err);
+    }
+  };
 
-    // Base64로 인코딩된 VAPID 공개 키를 Uint8Array로 변환하는 함수
-    const urlBase64ToUint8Array = (base64String) => {
-      const padding = "=".repeat((4 - (base64String.length % 4)) % 4);
-      const base64 = (base64String + padding)
-        .replace(/-/g, "+")
-        .replace(/_/g, "/");
-      const rawData = window.atob(base64);
-      const outputArray = new Uint8Array(rawData.length);
-      for (let i = 0; i < rawData.length; i++) {
-        outputArray[i] = rawData.charCodeAt(i);
-      }
-      return outputArray;
-    };
+  // ArrayBuffer를 Base64로 변환하는 함수
+  const arrayBufferToBase64 = (buffer) => {
+    let binary = "";
+    const bytes = new Uint8Array(buffer);
+    const len = bytes.byteLength;
+    for (let i = 0; i < len; i++) {
+      binary += String.fromCharCode(bytes[i]);
+    }
+    return window.btoa(binary);
+  };
 
-    fetchMyInfo();
-    handlePushNotification();
-  }, []);
+  // Base64로 인코딩된 VAPID 공개 키를 Uint8Array로 변환하는 함수
+  const urlBase64ToUint8Array = (base64String) => {
+    const padding = "=".repeat((4 - (base64String.length % 4)) % 4);
+    const base64 = (base64String + padding)
+      .replace(/-/g, "+")
+      .replace(/_/g, "/");
+    const rawData = window.atob(base64);
+    const outputArray = new Uint8Array(rawData.length);
+    for (let i = 0; i < rawData.length; i++) {
+      outputArray[i] = rawData.charCodeAt(i);
+    }
+    return outputArray;
+  };
 
   const handlePushOnOff = async () => {
     const headers = {
@@ -272,7 +267,7 @@ function MyInfoDetail() {
                 <input
                   type="checkbox"
                   checked={isPushEnabled}
-                  onChange={handlePushOnOff}
+                  onChange={handlePushNotification}
                 />
                 <Slider isEnabled={isPushEnabled}>
                   <SliderCircle isEnabled={isPushEnabled}></SliderCircle>
